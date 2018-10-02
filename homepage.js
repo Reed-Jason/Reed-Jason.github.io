63652e137882f5a188281e33155d882e13d6caf0
jjreed05 <jjreed05@gmail.com>
2018-10-02 09:04:40 -0600

    first commit

---
+++ homepage.js
@@ -0,0 +1,8 @@
+function insertTranslation() {
+    /* Romanized version of the Japanese */
+    document.getElementById('romaji').innerHTML = "Mezasubeki tokoro ni, chikamichi wa sonzai shinai.";
+    
+    /* English */
+    document.getElementById('english').innerHTML = "There are no short cuts to any place worth going";
+
+}
